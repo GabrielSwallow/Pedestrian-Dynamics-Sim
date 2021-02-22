a132153f12dfdd6cceff6e19164d43d310ac4063
GabrielSwallow <gabrielswallow@googlemail.com>
2021-02-22 16:31:01 +0000

    defined check for track being full

--- Propagate.py
+++ Propagate.py
@@ -1,3 +1,4 @@
 def track_time(agent, track):
     time = track.distance/agent.speed
     return time
+
--- Track.py
+++ Track.py
@@ -1,6 +1,30 @@
 class Track():
 
     def __init__(self, distance, max_capacity):
+        self.travellers = 0
         self.distance = distance
         self.max_capacity = max_capacity
         self.full = False
+
+    def update_full(self):
+        if self.travellers == self.max_capacity:
+            self.full = False
+        elif 0 <= self.travellers < self.max_capacity:
+            self.full = True
+        else:
+            raise Exception("negative number of travellers - spooky")
+    
+    def add_if_if_can(self, agent):
+        self.update_full
+        
+        if self.full == True:
+            self.travellers += 1
+        else:
+            return False
+    
+    def remove(self, agent):
+        self.update_full
+        self.travellers-=1
+        if self.travellers < 0:
+            raise Exception("negative number of travellers - spooky")
+        self.update_full
