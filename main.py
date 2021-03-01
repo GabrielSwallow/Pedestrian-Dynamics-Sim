3a765ee2c6957097d427d85414bc560a4450df58
Shivam Patel <shivam.patel19@imperial.ac.uk>
2021-03-01 16:27:12 +0000

    Added in extra node and track

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from Propagate import propagate
 from Track import Track
 
 Node1 = Node(max_capacity=5, is_end=False)
-Node2 = Node(max_capacity=5, is_end=True)
+Node2 = Node(max_capacity=5, is_end=False)
+Node3 = Node(max_capacity=5, is_end=True)
 agent1 = Agent(speed=5, initial_node=Node1)
-track = Track(distance=10, max_capacity=5, Node1=Node1, Node2=Node2, weight=1)
+track1 = Track(distance=10, max_capacity=5, Node1=Node1, Node2=Node2, weight=1)
+track2 = Track(distance=10, max_capacity=5, Node1=Node2, Node2=Node3, weight=1)
 
 agents = [agent1]
-tracks = [track]
+tracks = [track1, track2]
 
 propagate(agents, tracks)
